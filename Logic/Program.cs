765ff2e3b250bd62508fef3eccb35a149891ab85
Sean Pearce <seanpearce9722@gmail.com>
2025-11-21 18:54:13 -0600

    Completed an Assignment for the Microsoftw Full Stack Professional Certification Course

---
+++ Logic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+
+    static void Login()
+    {
+        string correctUsername = "Sean";
+        string correctPassword = "Kamehameha!";
+
+        Console.Write("Enter Username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter Password: ");
+        string password = Console.ReadLine();
+
+        if((username != correctUsername) && (password != correctPassword))
+        {
+            Console.Write("Invalid Input! Please Try Again!");
+            Login();
+        }
+
+        Console.Write("Welcome to the System!");
+    }
+
+    static void Main(string[] args)
+    {
+        Login();
+    }
+}
